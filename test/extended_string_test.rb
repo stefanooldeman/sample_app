c60ed1b09435ce0c7575968ce7edfa7f9d6d2458
stefanooldeman <stefano.oldeman@gmail.com>
2011-11-03 21:49:53 +0100

    test and add shuffle to string class

---
+++ test/extended_string_test.rb
@@ -0,0 +1,21 @@
+# require './test_helper.rb'
+require 'String'
+require 'test/unit'
+
+class TestStringExtension < Test::Unit::TestCase 
+
+  def is_implemented
+      assert_nothing_thrown("shuffle method should be implemented") { "hello".shuffle } 
+  end
+
+  def returns_string
+      assert_instance_of(String, "hello".shuffle, "shuffle should return string")
+  end
+
+  def same_length
+      expected = "hello".length
+      actualy = "hello".shuffle.length
+      assert_equal(expected, actually, "shuffled string should be same length")
+  end
+
+end
