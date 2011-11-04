4f1bfdb08baa206a521075d623bf3c512d2f9282
stefanooldeman <stefano.oldeman@gmail.com>
2011-11-04 09:37:43 +0100

    add view helper title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+    def title
+        base = "Ruby on Rails Tutorial Sample App"
+        if @title.nil?
+            base
+        else
+            "#{base} | #{@title}"
+        end
+    end
 end
