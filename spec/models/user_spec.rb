4611108a0c4c129eee7677ec67395bec9483e4e5
stefanooldeman <stefano.oldeman@gmail.com>
2011-12-30 12:35:53 +0100

    user should require email and name

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,23 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+    before :each do
+        # valid params
+        @params = {:name => "Example user", :email => "john@example.com"}
+    end
+
+    it "should create a new instance given valid attributes" do
+        User.create! @params
+    end
+
+    it "should require a name" do
+        user = User.new(@params.merge :name => "")
+        user.should_not be_valid
+    end
+
+    it "should require a email" do
+        user = User.new(@params.merge :email => "")
+        user.should_not be_valid
+    end
+
 end
