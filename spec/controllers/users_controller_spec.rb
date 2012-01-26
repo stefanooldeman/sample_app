c337e591fe6a31bd63efeb5a0ed41bdd8e513298
stefanooldeman <stefano.oldeman@gmail.com>
2012-01-26 15:15:30 +0100

    fix factory user and test :show action user_controller

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -4,15 +4,33 @@ describe UsersController do
     #note this line:
     render_views
 
+    describe "GET 'show'" do
+
+        before(:each) do
+            @user = Factory(:user)
+        end
+
+        it "should be successful" do
+            get :show, :id => @user
+            response.should be_success
+        end
+
+        it "should find the right user" do
+            get :show, :id => @user
+            assigns(:user).should == @user
+        end
+    end
+
     describe "GET 'new'" do
+
         it "should be success" do
-          get 'new'
+          get :new
           response.should be_success
         end
 
         it "should have the right title" do
             base_title = "Ruby on Rails Tutorial Sample App |"
-            get 'new'
+            get :new
             response.should have_selector "title", :content => base_title + " Sign up"
         end
     end
